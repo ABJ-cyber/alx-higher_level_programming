38d50a570af800a50bb6487001127fdfeb82e4a1
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-09-23 11:49:34 -0700

    argsv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,11 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 2:
-        print(f"{1:d} argument")
-        print(f"{1:d} : {sys.argv[1]}")
+        print(f"{1:d} argument:")
+        print(f"{1:d}: {sys.argv[1]}")
+    elif len(sys.argv) == 1:
+        print(f"{0:d} arguments.")
     else:
-        print("{:d} arguments".format(len(sys.argv) - 1))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print(f"{i:d}: {sys.argv[i]}")
