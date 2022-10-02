de960bb1ec2cc3db4ea85926b9851cdf0086c711
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-10-02 08:56:26 -0700

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,8 @@
 def no_c(my_string):
     new = ""
     for letter in my_string:
-        if letter != "C" or letter != 'c':
+        if letter == "C" or letter == 'c':
+            pass
+        else:
             new += letter
     return new
